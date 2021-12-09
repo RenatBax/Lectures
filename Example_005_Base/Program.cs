49f68906f5eebe8cd39d33fc3094e7e1e9f8443b
RenatBax <renchik_125.111@mail.ru>
2021-12-09 19:44:37 +0300

    Пятый пример задача с гирями

---
+++ Example_005_Base/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача с гирями
+
+int a = 4;
+int b = 2;
+int c = 8;
+int d = 1;
+int e = 6;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+Console.WriteLine($"max = {max}");
