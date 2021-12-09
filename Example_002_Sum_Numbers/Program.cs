13b78a7d66933d9a58c1ef6a9b81dcbb34b0cf60
RenatBax <renchik_125.111@mail.ru>
2021-12-09 18:42:39 +0300

    Второй пример сумма дыух чисел

---
+++ Example_002_Sum_Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу на языке С#
+// для Windows и MacOS в которую пользователь 
+// вводит два целых числа?
+// в которой записаны два целых? числа?
+// два целых? числа генерируются случайным образом?
+// и получает сумму
+// АЛГОРИТМ
+// Начало
+// Определить numberA
+// Определить numberB
+// Вывод numberA + numberB
+// Конец
+
+int namberA = 3;
+int namberB = 5;
+Console.WriteLine(namberA + namberB);
+
+int result = namberA + namberB;
+Console.WriteLine(result);
