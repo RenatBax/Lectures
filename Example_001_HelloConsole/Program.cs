7a08994bdc323c2b041ef682b62e171a50683942
RenatBax <renchik_125.111@mail.ru>
2021-12-09 18:23:49 +0300

    Первый пример, привествие пользователя

---
+++ Example_001_HelloConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿// АЛГОРИТМ
+// начало 
+// ввод "username" 
+// вывод "Привет" 
+// вывод "username" 
+// конец
+
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("привет, ");
+Console.Write(username);
