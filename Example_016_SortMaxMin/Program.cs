870aa8295d8d2c52eaa540e59d04dd583dc8c050
RenatBax <renchik_125.111@mail.ru>
2021-12-14 01:01:54 +0300

    Домашнее задание сортировка выбором от мах до мин

---
+++ Example_016_SortMaxMin/Program.cs
@@ -0,0 +1,40 @@
+﻿// Сортировка выбором от мах к мин
+
+void CreateArray(int[] array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(1, 21);
+        
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] +" ");
+    }
+}
+void SelectionSort(int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        int maxPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition])
+            maxPosition = j;
+        }
+        int place = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = place;
+    }
+}
+
+int[] arr = new int[10];
+CreateArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+SelectionSort(arr);
+PrintArray(arr);
