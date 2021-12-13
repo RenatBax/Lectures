01f233c44270ff8aaf4da51c1988da677e2da8f3
RenatBax <renchik_125.111@mail.ru>
2021-12-13 16:22:48 +0300

    Пример по созданию массива с помощью методов void

---
+++ Example_010_ArrayLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿// Методы заполнения массива и его показ на экране
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] position)
+{
+    int size = position.Length;
+    int count = 0;
+    while (count < size)
+    {
+        Console.Write(position[count] +" ");
+        count++;
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
