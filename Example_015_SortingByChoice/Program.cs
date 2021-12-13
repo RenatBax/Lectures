64063cf71e5550f20364aa868939a994cbee16f5
RenatBax <renchik_125.111@mail.ru>
2021-12-14 00:18:09 +0300

    Пример цикла в цикле в сортировке выбором от мин к максу

---
+++ Example_015_SortingByChoice/Program.cs
@@ -0,0 +1,30 @@
+﻿// Алгоритм сортировка выбором
+int[] arr = { 1, 4, 6, 8, 4, 9, 7, 3, 2, 1 };
+void PrintArray(int[] array) //метод показа массива на экран
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array) //метод сортировки массива выбором
+{
+    for(int i = 0; i < array.Length - 1; i++) //исскуственно отнимаем, тк в 17 строке мы прибавляли то здесь искуственно отнимаем
+    {
+        int minPosition = i;// запоминаем
+        for(int j = i + 1; j < array.Length; j++) // здесь ищем мах элемент, начальная позиция есть поэтому i+1
+        {
+           if(array[j] < array[minPosition])
+           minPosition = j; //сохраням позицию текущего элементы
+        }
+        int temporary = array[i]; // меняем местами
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
