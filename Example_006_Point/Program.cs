94dd365e5032a373ea68c8256c9eefddee5b0a4b
RenatBax <renchik_125.111@mail.ru>
2021-12-13 11:02:23 +0300

    Шестой пример, нахождение средней точки на отрезке

---
+++ Example_006_Point/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача про точки
+// АЛГОРИТМ
+// Определить три точки
+// Выбрать две любых
+// Найти середину между этими точками
+// Поставить точку
+// Выбрать случайную вершину треугольника
+// Соединить ее с полученной на 4 шаге точкой
+// Перейти к шагу 3
+// Шаги 3-7 повторять 9, 28, 32 раза
+
+Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+int count = 0;
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
