1465ea00127849bb2121ece9f4f3318f749244c8
RenatBax <renchik_125.111@mail.ru>
2021-12-16 02:31:38 +0300

    Добавлена пример с рекурсией

---
+++ Example_019_Recursion/Program.cs
@@ -0,0 +1,35 @@
+﻿// Рекурсия 
+// Нахождение факториала
+//5! = 5*
+
+// double Factorial (double n)
+// {
+//     //1! = 1
+//     //0! = 1
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// Console.WriteLine(Factorial(4)); //1*2*3 = 6
+
+// for (double i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+// числа Фибоначи 
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+// double Fibonacci (int n)
+// {
+//     if(n == 1 || n == 2) return 1;
+//     else return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+// for(int i = 1; i < 40; i++)
+// {
+//     //Console.WriteLine(Fibonacci(i));
+//     Console.WriteLine($"f({i})! = {Fibonacci(i)}");
+// }
+
+// обойти директорию ???апи???
