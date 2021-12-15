28e5b0cd98fe7488e1825ac5e14cdabbd94d7d76
RenatBax <renchik_125.111@mail.ru>
2021-12-16 02:23:08 +0300

    Добавлена пример с закрашиванием области

---
+++ Example_017_Like/Program.cs
@@ -0,0 +1,32 @@
+﻿// 
+int[,] pic = new int[23, 25];
+//int[,] pic = new int[,];
+void PrintImage(int[,] image) //метод показа на экран
+{
+    for (int row = 0; row < image.GetLength(0); row++) 
+    {
+        for ( int col = 0; col < image.GetLength(1); col++)
+        {
+            //Console.Write($"{image[i, j]} ");
+            if (image[row, col] == 0) Console.Write($"0");
+            Console.Write($"+");
+        }
+        Console.WriteLine();
+
+    }
+}  
+// void FillImage(int row, int col) // метод заполнения
+// {
+//     if (pic[row, col] == 0) 
+//     {
+//         pic[row, col] = 1;
+//         FillImage(row-1, col);
+//         FillImage(row, col-1);
+//         FillImage(row+1, col);
+//         FillImage(row, col+1);
+//     }
+// }  
+
+PrintImage(pic); 
+// FillImage(12, 12);
+// PrintImage(pic);
